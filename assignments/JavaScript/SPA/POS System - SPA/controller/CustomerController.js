df5daa2650a521a6cf8534f975dabe2fd94be536
TharuZika <maxtharuz4@gmail.com>
2022-10-12 16:35:56 +0530

    Configure RegEx

--- assignments/JavaScript/SPA/POS System - SPA/controller/CustomerController.js
+++ assignments/JavaScript/SPA/POS System - SPA/controller/CustomerController.js
@@ -152,4 +152,122 @@ function deleteCustomer(customerID) {
     }
 }
 
+// Customer Validation process
+
+const customerIDRegEx = /^(C)[0-9]{3}$/;
+const customerNameRegEx = /^[A-z ]{5,20}$/;
+const customerAddrRegEx = /^[0-9/A-z. ,]{5,}$/;
+const customerContRegEx = /^(0)[0-9]{9}$/;
+
+let customerValidations = [];
+customerValidations.push({
+    reg: customerIDRegEx,
+    field: $('#cNo'),
+    error: 'Customer ID Pattern is Wrong : C000',
+    cato: "add"
+});
+customerValidations.push({
+    reg: customerNameRegEx,
+    field: $('#cName'),
+    error: 'Customer Name Pattern is Wrong : A-z 5-20',
+    cato: "add"
+});
+customerValidations.push({
+    reg: customerAddrRegEx,
+    field: $('#cAddress'),
+    error: 'Customer Address Pattern is Wrong : 0-9',
+    cato: "add"
+});
+customerValidations.push({
+    reg: customerContRegEx,
+    field: $('#cContact'),
+    error: 'Customer Contact Pattern is Wrong : 100 or 100.00',
+    cato: "add"
+});
+customerValidations.push({
+    reg: customerIDRegEx,
+    field: $('#ucust-no'),
+    error: 'Customer ID Pattern is Wrong : C000',
+    cato: "update"
+});
+customerValidations.push({
+    reg: customerNameRegEx,
+    field: $('#uname'),
+    error: 'Customer Name Pattern is Wrong : A-z 5-20',
+    cato: "update"
+});
+customerValidations.push({
+    reg: customerAddrRegEx,
+    field: $('#uaddress'),
+    error: 'Customer Address Pattern is Wrong : 0-9',
+    cato: "update"
+});
+customerValidations.push({
+    reg: customerContRegEx,
+    field: $('#ucust-Contact'),
+    error: 'Customer Contact Pattern is Wrong : 100 or 100.00',
+    cato: "update"
+});
+
+function defaultAllTextCustomer() {
+    for (let validation of customerValidations) {
+        validation.field.css("border", "1px solid #ced4da");
+        validation.field.parent().children('span').text("");
+    }
+
+}
+
+function defaultTextCustomer(txtField, error) {
+    txtField.css("border", "1px solid #ced4da");
+    txtField.parent().children('span').text(error);
+}
+
+function checkCustomerInput(regex, txtField) {
+    let inputVal = txtField.val();
+    return regex.test(inputVal) ? true : false;
+}
+
+function btnStateCustomer(txtType, stat) {
+    if (txtType == "add") {
+        if (stat == "success") {
+            $('#btnAddCustomer').attr('disabled', false);
+        } else if (stat == "fail") {
+            $('#btnAddCustomer').attr('disabled', true);
+        }
+    } else if (txtType == "update") {
+        if (stat == "success") {
+            $('#btnCusUpdate').attr('disabled', false);
+        } else if (stat == "fail") {
+            $('#btnCusUpdate').attr('disabled', true);
+        }
+    }
+}
+
+function checkCustomerValidity() {
+    for (let validation of customerValidations) {
+        if (checkCustomerInput(validation.reg, validation.field)) {
+            if (validation.field.val().length <= 0) {
+                defaultTextCustomer(validation.field, "");
+            } else {
+                validation.field.css('border', '2px solid green');
+                validation.field.parent().children('span').text("");
+                btnStateCustomer(validation.cato, "success");
+            }
+        } else {
+            if (validation.field.val().length <= 0) {
+                defaultTextCustomer(validation.field, "");
+            } else {
+                validation.field.css('border', '2px solid red');
+                validation.field.parent().children('span').text(validation.error);
+                btnStateCustomer(validation.cato, "fail");
+            }
+        }
+    }
+}
+
+$("#cNo,#cName,#cAddress,#cContact,#ucust-no,#ucust-Contact,#uname,#uaddress").on('keyup', function (event) {
+    checkCustomerValidity();
+});
+
+
 
